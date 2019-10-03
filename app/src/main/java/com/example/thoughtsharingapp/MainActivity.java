25fc3baaab05299b1a9751a3bbe9586508bd1829
Kenneth Kouadio <kennethemmanuelziguy28@gmail.com>
2019-10-03 21:47:56 +0300

    add fake list_view

--- app/src/main/java/com/example/thoughtsharingapp/MainActivity.java
+++ app/src/main/java/com/example/thoughtsharingapp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.thoughtsharingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> thoughts;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list_view);
+        thoughts = new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        thoughts.add("To be Changed");
+        ThoughtInfoAdapter adapter = new ThoughtInfoAdapter(this, thoughts);
+        listView.setAdapter(adapter);
     }
 }
