d2aaeeac5c09d195eeac936ab5a813312c116646
Chakrapanikurra <chakra.kurra@gmail.com>
2019-11-07 16:49:41 -0800

    added the post activity

--- app/src/main/java/com/example/thoughtsharingapp/Feed.java
+++ app/src/main/java/com/example/thoughtsharingapp/Feed.java
@@ -2,6 +2,7 @@ package com.example.thoughtsharingapp;
 
 public class Feed {
     private String postText;
+    private String postTitle;
 
     public Feed() {
 
@@ -17,5 +18,13 @@ public class Feed {
 
     public void setPostText(String postText) {
         this.postText = postText;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
     }
 }
--- app/src/main/java/com/example/thoughtsharingapp/PostMessageActivity.java
+++ app/src/main/java/com/example/thoughtsharingapp/PostMessageActivity.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PostMessageActivity extends AppCompatActivity {
+
+    private EditText titleEditext;
+    private EditText textEditext;
+
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_message);
+        titleEditext = findViewById(R.id.title_edit_text);
+        textEditext = findViewById(R.id.text_edit_text);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Posts");
+
 
 
 
@@ -23,8 +34,26 @@ public class PostMessageActivity extends AppCompatActivity {
 
     public void postTitle(View view){
 
-        MainActivity mainActivity =  new MainActivity<>();
-        mainActivity.startPosting();
+        if(!titleEditext.getText().toString().isEmpty() && !textEditext.getText().toString().isEmpty()){
+        startPosting(titleEditext.getText().toString(),textEditext.getText().toString());
+        }
+        finish();
+
+    }
+
+    // This method posts to the database
+    private void startPosting(String title,String text) {
+
+
+        /**DatabaseReference newPost = databaseReference.push();
+         newPost.child("post").setValue(postInput); */
+        // Write a message to the database
+        DatabaseReference newPost = databaseReference.push();
+        newPost.child("postTitle").setValue(title);
+        newPost.child("postText").setValue(text);
+
+
+
 
     }
 
